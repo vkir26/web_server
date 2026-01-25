04ed3234c9c56aba62fcd69a7a24aa4dd15d07f9
vkir26 <vanya.kireew2012@yandex.ru>
2026-01-25 16:37:53 +0300

    Creating the first version of the web server

--- web_server.py
+++ web_server.py
@@ -1,2 +1,11 @@
-def start_server() -> None:
-    pass
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("127.0.0.1", 8000))
+server.listen(3)
+client_socket, client_address = server.accept()
+print("Connected by", client_address)
+data = client_socket.recv(1024).decode("UTF-8")
+print(data)
+html_message = b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello World!"
+client_socket.send(html_message)
