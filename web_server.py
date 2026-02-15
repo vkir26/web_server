ab497403e9f5de8fc186e92813af1e96b6d836db
vkir26 <vanya.kireew2012@yandex.ru>
2026-02-15 17:38:26 +0300

    Fixing after review

--- web_server.py
+++ web_server.py
@@ -1,12 +1,22 @@
-from enum import IntEnum
+from http import HTTPStatus
 from typing import Union
 from dataclasses import dataclass
 import socket
 
 
-def read_root() -> socket.socket:
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("127.0.0.1", 8000))
+@dataclass(frozen=True, slots=True)
+class ServerAddress:
+    ip: str
+    port: int
+
+
+def read_root(
+    address_type: socket.AddressFamily | int,
+    protocol_type: socket.SocketKind | int,
+    server_address: ServerAddress,
+) -> socket.socket:
+    server = socket.socket(address_type, protocol_type)
+    server.bind((server_address.ip, server_address.port))
     server.listen(10)
     return server
 
@@ -16,93 +26,91 @@ def read_item(item_id: int, q: Union[str, None] = None) -> dict[str, int | str |
 
 
 @dataclass(frozen=True, slots=True)
+class RequestTarget:
+    path: str
+    param: str
+
+
+@dataclass(frozen=True, slots=True)
 class RequestLine:
     method: str
-    path: str
+    request_target: RequestTarget
     version: str
 
 
-class HttpStatusCode(IntEnum):
-    Success = 200
-    BadRequest = 400
-    NotFound = 404
+def get_status(target_path: str) -> tuple[HTTPStatus, dict[str, int]]:
+    path_params: dict[str, int] = {}
+    if target_path == "/":
+        return HTTPStatus.OK, path_params
+    if target_path.startswith("/items/"):
+        path_parts = target_path.strip("/").split("/")
+        if len(path_parts) != 2 or not path_parts[1].isdigit():
+            return HTTPStatus.BAD_REQUEST, path_params
+        path_params["item_id"] = int(path_parts[1])
+        return HTTPStatus.OK, path_params
+    return HTTPStatus.NOT_FOUND, path_params
 
 
-def get_status(request_line: RequestLine) -> tuple[HttpStatusCode, dict[str, int]]:
-    path_only = request_line.path.split("?", 1)[0]
-    path_params = {}
-    if path_only == "/":
-        status = HttpStatusCode.Success
-    elif path_only.startswith("/items/"):
-        try:
-            pathname, item_id = path_only.split("/")[1:3]
-            path_params["item_id"] = int(item_id)
-        except ValueError:
-            status = HttpStatusCode.BadRequest
-        else:
-            status = HttpStatusCode.Success
-    else:
-        status = HttpStatusCode.NotFound
-    return status, path_params
-
-
-def get_query(path: str) -> dict[str, str]:
+def get_query(target_param: str) -> dict[str, str]:
     query_params = {}
-    if "?" in path:
-        for i in path.split("?")[1].split("&"):
-            if "=" in i:
-                key, value = i.split("=", 1)
-                query_params[key] = value
-            elif i:
-                key, value = i, ""
-                query_params[key] = value
+    for q in target_param.split("&"):
+        if "=" in q:
+            key, value = q.split("=", 1)
+        else:
+            key, value = q, ""
+        query_params[key] = value
     return query_params
 
 
-def get_response(status: int) -> str:
-    status_text = {
-        HttpStatusCode.Success: "OK",
-        HttpStatusCode.BadRequest: "Bad Request",
-        HttpStatusCode.NotFound: "Not Found",
-    }
-    return (
-        f"HTTP/1.1 {status} {status_text[HttpStatusCode(status)]}\r\n"
-        + "Content-Type: text/html\r\n\r\n{}"
-    )
+def get_response(
+    http_ver: str, status: int, response: dict[str, int | str | None]
+) -> str:
+    return f"{http_ver} {status} {HTTPStatus(status).phrase}\r\nContent-Type: text/html\r\n\r\n{response}"
 
 
 def path_handler(request_line: RequestLine) -> bytes:
-    path_only = request_line.path.split("?", 1)[0]
-    status, path_params = get_status(request_line=request_line)
-    query_params = get_query(request_line.path)
-    http_response = get_response(status)
+    target = request_line.request_target
+    status, path_params = get_status(target_path=target.path)
     html_message = ""
     match status:
-        case HttpStatusCode.Success:
-            if path_only == "/":
-                html_message = http_response.format({"Hello": "World"})
+        case HTTPStatus.OK:
+            if target.path == "/":
+                html_message = get_response(
+                    request_line.version, status, {"Hello": "World"}
+                )
             elif path_params.get("item_id") is not None:
+                query_params = get_query(target.param)
                 body = read_item(
                     item_id=path_params["item_id"], q=query_params.get("q")
                 )
-                html_message = http_response.format(body)
-        case HttpStatusCode.BadRequest:
-            html_message = http_response.format({"detail": "Bad Request"})
-        case HttpStatusCode.NotFound:
-            html_message = http_response.format({"detail": "Not Found"})
+                html_message = get_response(request_line.version, status, body)
+        case HTTPStatus.BAD_REQUEST | HTTPStatus.NOT_FOUND:
+            html_message = get_response(
+                request_line.version, status, {"detail": HTTPStatus(status).phrase}
+            )
     return bytes(html_message, encoding="UTF-8")
 
 
 def main() -> None:
-    server = read_root()
+    address_type = socket.AF_INET
+    protocol_type = socket.SOCK_STREAM
+    server_address = ServerAddress(ip="127.0.0.1", port=8000)
+    server = read_root(address_type, protocol_type, server_address)
     while True:
         client_socket, client_address = server.accept()
         print("Connected by", client_address)
         data = client_socket.recv(1024)
-        request_line = RequestLine(
-            *data.decode("UTF-8").strip().split("\r\n")[0].split()
+        method, target, version = data.decode("UTF-8").strip().split("\r\n")[0].split()
+        if "?" in target:
+            request_target = RequestTarget(*target.split("?"))
+        else:
+            request_target = RequestTarget(path=target, param="")
+        server_response = path_handler(
+            request_line=RequestLine(
+                method=method, request_target=request_target, version=version
+            )
         )
-        client_socket.send(path_handler(request_line))
+        client_socket.send(server_response)
         client_socket.close()
 
 
